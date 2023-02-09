1be69311ed7f4337f2a1810dbbf8ca33e9c19108
yunyunfunnydays <yunhsuan2017@gmail.com>
2023-02-09 22:02:18 +0800

    add login api

---
+++ myPractice/login.js
@@ -0,0 +1,27 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+const app = createApp({
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    login() {
+      const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+      axios
+        .post(api, this.user)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((res) => {
+          console.error(res);
+        });
+    },
+  },
+});
+
+app.mount('#app');

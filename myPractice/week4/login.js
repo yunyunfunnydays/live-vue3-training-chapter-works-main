68279c331b2713987f6c26d452d867e99bf2b37c
yunyunfunnydays <yunhsuan2017@gmail.com>
2023-02-14 21:25:26 +0800

    update week4 login

--- myPractice/week4/login.js
+++ myPractice/week4/login.js
@@ -1,25 +1,29 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
 const app = createApp({
-  data() {
-    return {
-      user: {
-        username: '',
-        password: '',
-      },
-    };
-  },
-  methods: {
-    login() {
-      const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
-      axios.post(api, this.user).then((res) => {
-        console.log(res);
-        const {token, expired} = res.data;
-        document.cookie = `hexToken = ${token}; expires=${new Date(expired)}; path=/`;
-        window.location = 'products.html'
-      }).catch((err) => {
-        alert(err.data.message);
-      });
-    },
-  },
+	data() {
+		return {
+			test: 'test',
+			user: {
+				username: '',
+				password: '',
+			}
+		}
+	},
+	methods: {
+		login() {
+			const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+			axios.post(api, this.user)
+				.then((res) => {
+					const { token, expired } = res.data;
+					document.cookie = `token=${token};expires=${new Date(expired)};path=/`;
+					window.location = 'product.html';
+				})
+				.catch((err) => {
+					alert(err.response.data.message);
+				})
+		}
+	}
 });
+
 app.mount('#app');
